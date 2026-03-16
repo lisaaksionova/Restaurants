4176235333e123ea8095142c7f14910b9f810228
Yelyzaveta Aksionova <lisaaksionova@gmail.com>
2026-03-16 23:09:07 +0200

    add db

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,4 +1,5 @@
 using Restaurants.API.Controllers;
+using Restaurants.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<IWeatherForecastService, WeatherForecastService>();
+
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
