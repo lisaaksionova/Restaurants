bbf4925eebe926a54b5abb41ee31b5f41f4e07b5
Yelyzaveta Aksionova <lisaaksionova@gmail.com>
2026-03-17 21:20:09 +0200

    add new libraries

---
+++ Restaurants.Application/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Restaurants.Application.Restaurants;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+
+namespace Restaurants.Application.Extensions;
+
+public static class ServiceCollectionExtension
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IRestaurantsService, RestaurantsService>();
+        services.AddAutoMapper(cfg => { }, typeof(ServiceCollectionExtension).Assembly);
+        services.AddValidatorsFromAssembly(typeof(ServiceCollectionExtension).Assembly)
+            .AddFluentValidationAutoValidation();
+    }
+}
